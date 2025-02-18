251e2217465d0593b9bb008ed46ba5b161479240
Kusk24 <winyumg2003a@gmail.com>
2025-02-18 20:49:57 +0700

    Changes model usage
    Co-authored-by: Soe Phone Pyae <spp200802ygn@gmail.com>
    Co-authored-by: Thant Zin Min <thantzinmin.us@gmail.com>

--- model/Banned_members.py
+++ model/Banned_members.py
@@ -1,13 +1,16 @@
 from database import db 
 from datetime import date
 
-class Banned_Members(db.Model):
-    __tablename__ = 'banned_members'
-    Ban_Id = db.Column(db.Integer, autoincrement = True)
-    Customer_Id = db.Column(db.Integer, db.ForeignKey('customer.Customer_Id'), nullable=False)
-    Reason = db.Column(db.Text, nullable = False)
-    Ban_Date = db.Column(db.Date, default = date.today)
+class Banned_members(db.Model):
+    __table__ = db.Table('banned_members', db.metadata, autoload_with=db.engine)
+
+# class Banned_Members(db.Model):
+#     __tablename__ = 'banned_members'
+#     Ban_Id = db.Column(db.Integer, autoincrement = True)
+#     Customer_Id = db.Column(db.Integer, db.ForeignKey('customer.Customer_Id'), nullable=False)
+#     Reason = db.Column(db.Text, nullable = False)
+#     Ban_Date = db.Column(db.Date, default = date.today)
     
-    __table_args__ = (
-        db.PrimaryKeyConstraint('Ban_Id', 'Customer_Id'),
-    )
+#     __table_args__ = (
+#         db.PrimaryKeyConstraint('Ban_Id', 'Customer_Id'),
+#     )
--- model/Game.py
+++ model/Game.py
@@ -1,4 +1,7 @@
 from database import db 
 
 class Game(db.Model):
-    __tablename__ = "game"
+    __table__ = db.Table('game', db.metadata, autoload_with=db.engine)
+    
+# class Game(db.Model):
+#     __tablename__ = "game"
