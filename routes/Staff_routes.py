45230ee779d2ac84e267fa2965bba26814e5bf36
Soe Phone Pyae <sky@Soes-MacBook-Air-2.local>
2025-02-24 21:06:50 +0700

    Staff create endpoint added

--- routes/Staff_routes.py
+++ routes/Staff_routes.py
@@ -1,6 +1,7 @@
-from flask import jsonify
+from flask import jsonify, request
 from app import app, db
 from model import Staff
+from datetime import datetime
 
 @app.route("/staff", methods=['GET'])
 def get_staff():
@@ -18,3 +19,32 @@ def get_staff():
                      "type" : s.type,
                      "admin_id" : s.admin_id,
                      "last_action" : s.last_action} for s in staff])
+
+
+@app.route("/staff", methods=['POST'])
+def create_game():
+    data = request.get_json()
+    required_fields = ['staff_name', 'email', 'password', 'phone_number', 'salary', 
+                       'street_address', 'city', 'state', 'zip_code', 'type', 'admin_id']
+    if not all(field in data for field in required_fields):
+        return jsonify({"error": "Missing required fields"}), 400
+    
+    new_staff = Staff(
+        staff_name = data["staff_name"],
+        email = data["email"],
+        password = data['password'],
+        phone_number = data['phone_number'],
+        salary = data['salary'],
+        street_addess = data['street_address'],
+        city = data['city'],
+        state = data['state'],
+        zip_code = data['zip_code'],
+        type = data['type'],
+        admin_id = data['admin_id'],
+        last_action = datetime.today().strftime('%Y-%m-%d')
+    )
+
+    db.session.add(new_staff)
+    db.session.commit()
+    return jsonify({"message": "Game added"})
+        
